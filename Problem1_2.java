523365d39f1389866a6bf188037351537890ffa3
sslevel5 <sslevel5@gmail.com>
2024-06-17 19:24:03 +0900

    練習

---
+++ Problem1_2.java
@@ -0,0 +1,9 @@
+public class Problem1_2 {
+ 
+    public static void main(String[] args) {
+        System.out.print("ABC");
+        System.out.print("DEF");
+        System.out.println("GHI");
+    }
+ 
+}
