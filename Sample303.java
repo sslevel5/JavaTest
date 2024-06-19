0963ce9b9e3202aa441f49cba29ca6e78473f1d2
sslevel5 <sslevel5@gmail.com>
2024-06-19 19:53:38 +0900

    305

---
+++ Sample303.java
@@ -0,0 +1,17 @@
+public class Sample303 {
+ 
+    public static void main(String[] args) {
+        //標準出力
+        int num = 10;                    //  整数値（いろいろ変えてみましょう）
+        if(num == 1){
+            System.out.println("one");    //  numが1だった場合の処理
+        }else if(num == 2){
+            System.out.println("two");    //  numが2だった場合の処理
+        }else if(num == 3){
+            System.out.println("three");  //  numが3だった場合の処理
+        }else{
+            System.out.println("不適切な値です。"); //  それ以外の値が入力された場合の処理
+        }
+    }
+ 
+}
