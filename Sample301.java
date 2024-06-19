f074c8fec034aabf3a0d0ab6184eee25efacf4bf
sslevel5 <sslevel5@gmail.com>
2024-06-19 19:13:43 +0900

    302

---
+++ Sample301.java
@@ -0,0 +1,13 @@
+public class Sample301 {
+ 
+    public static void main(String[] args) {
+        //標準出力
+        int a = 10; //  整数値（いろいろ変えてみましょう）
+        System.out.println("a="+a);
+        //  入力した値が、正の数かどうかを調べる
+        if(a > 0){
+            System.out.println("aは正の数です。");  //  正の数だった場合に実行
+        }
+    }
+ 
+}
