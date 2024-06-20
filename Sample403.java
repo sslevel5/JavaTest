1ee3472411d71386291101afcdaa01c4d8493d32
sslevel5 <sslevel5@gmail.com>
2024-06-20 14:34:54 +0900

    404

---
+++ Sample403.java
@@ -0,0 +1,11 @@
+public class Sample403 {
+ 
+    public static void main(String[] args) {
+       int i = 0;
+       while(i <= 5){
+           System.out.print(i+" ");
+           i++;
+       }
+       System.out.println();
+    }
+}
---
+++ Sample404.java
@@ -0,0 +1,11 @@
+public class Sample404 {
+ 
+    public static void main(String[] args) {
+       int i = 0;
+       do{
+           System.out.print(i+" ");
+           i++;
+       }while(i <= 5);
+       System.out.println();
+    }
+}
